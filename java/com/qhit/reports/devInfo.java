6ce08195df1a252188ff7428f7e313e458d709df
Mr.wang <34223819+git-Mr-wang@users.noreply.github.com>
2019-04-25 19:05:43 +0800

    Add files via upload

--- java/com/qhit/reports/devInfo.java
+++ java/com/qhit/reports/devInfo.java
@@ -1,5 +1,6 @@
 package com.qhit.reports;
 
+import com.qhit.baseUser.pojo.BaseUser;
 import com.qhit.produceReport.service.IProduceReportService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +21,55 @@ public class devInfo {
     @Resource
     IProduceReportService produceReportService;
 
+    /**
+     * 港口间设备利用率对比
+     * @param year
+     * @return
+     */
     @RequestMapping("/usage")
-    public Object usage(String year, HttpSession session){
-        Map map = new HashMap();
-        String[] columns = {"compname","devType","ratio"};
-        List<Map> rows = produceReportService.usage(year);
-        map.put("columns",columns);
-        map.put("rows",rows);
-        return  map;
+    public Map<String,Object> usage(String year){
+        return produceReportService.usage(year);
+    }
+
+    /**
+     * 2、港口间设备完好率对比：
+     * @param year
+     * @return
+     */
+    @RequestMapping("/intactRatio")
+    public Map<String,Object> intactRatio(String year){
+        return produceReportService.intactRatio(year);
+    }
+
+    /**
+     * 成本核算信息：
+     * @param year
+     * @param session
+     * @return
+     */
+    @RequestMapping("/cost")
+    public List<Map> cost(String year, HttpSession session){
+        BaseUser baseUser = (BaseUser) session.getAttribute("baseUser");
+        return produceReportService.cost(year,baseUser.getCompid());
+    }
+
+    /**
+     * 港口间作业量对比
+     * @param year
+     * @return
+     */
+    @RequestMapping("/amount")
+    public Map<String,Object> amount(String year){
+        return produceReportService.amount(year);
+    }
+
+    /**
+     * 港口间能耗对比
+     * @param year
+     * @return
+     */
+    @RequestMapping("/consume")
+    public Map<String,Object> consume(String year){
+        return produceReportService.consume(year);
     }
 }
